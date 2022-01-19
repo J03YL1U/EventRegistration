3043258b87ce32955c00d2a535bd9dc87fafae5d
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2022-01-19 16:09:38 -0500

    hello

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -13,7 +13,7 @@ public class EventRegistrationBackendApplication {
     SpringApplication.run(EventRegistrationBackendApplication.class, args);
   }
 
-  // Request Maps
+  // Request Map
   @RequestMapping("/")
   public String greeting(){
     return "Hello world!";
